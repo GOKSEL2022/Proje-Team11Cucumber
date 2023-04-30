c0ecdfc9f2761df2a32848465f4d4b5d30f0612d
atulubas <ali.tulubas2016@gmail.com>
2023-04-30 23:47:33 +0300

    30nisan

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",//features folder path
         glue = {"stepdefinitions","hooks"},   //stepdefinitions path
 
-        tags = "@DBtest",
+        tags = "@US05_AC03_TC09",
 
 
 
